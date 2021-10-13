b27cf5a0c5fd1dc48860a34e88aeee5ace8638da
NoemiJ01 <89852801+NoemiJ01@users.noreply.github.com>
2021-10-13 12:24:11 -0400

    Today's work:)

--- src/components/photos.js
+++ src/components/photos.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import dog1 from './images/dog1.jpg';
-import cat from './images/cat.jpg';
-import dog2 from './images/dog2.jpg';
-import './css/photos.css';
-
+import React from "react";
+import dog1 from "./images/dog1.jpg";
+import cat from "./images/cat.jpg";
+import dog2 from "./images/dog2.jpg";
+import "./css/photos.css";
+import dogs3 from "./images/dogs3.jpeg";
 
 function Photos() {
-  return(
-    <div className='container-fluid Photos'>
-      <div className='row'>
-        <div className='col'>
-          <img src={ cat } alt="cat"/>
+  return (
+    <div className="container-fluid Photos">
+      <div className="row">
+        <div className="col">
+          <img src={cat} alt="cat" />
         </div>
-        <div className='col'>
-          <img src={ dog2 } alt="small dog"/>
+        <div className="col">
+          <img src={dog2} alt="small dog" />
         </div>
-        <div className='col'>
-          <img src={ dog1 } alt="large dog"/>
+        <div className="col">
+          <img src={dog1} alt="large dog" />
         </div>
       </div>
     </div>
